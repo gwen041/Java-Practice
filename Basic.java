cacb8e7866f692f13d965b4b0adad2131bc0f327
gwen041 <almiratabios01@gmail.com>
2025-12-30 22:35:18 +0800

    added in sums

--- Basic.java
+++ Basic.java
@@ -5,7 +5,14 @@ public class Basic {
         System.out.print(name);
         System.out.println("Hello\n" + name);*/
 
+        //Addition of two num
         int num1 = 74, num2 = 36;
-        System.out.println("Sum is " + (num1 + num2));
+        int sum = num1 + num2;
+        System.out.println("Sum is " + sum);
+        //System.out.println("Sum is " + (num1 + num2));
+
+
+
+
     }
 }
