134b78aa68b3eaf38fe62228e255ba981511ed9c
gwen041 <almiratabios01@gmail.com>
2025-12-30 22:32:13 +0800

    sums

---
+++ Basic.java
@@ -0,0 +1,11 @@
+public class Basic {
+    public static void main(String[] args) {
+        /*String name = "Gwen Tabios";
+        System.out.println("Hello");
+        System.out.print(name);
+        System.out.println("Hello\n" + name);*/
+
+        int num1 = 74, num2 = 36;
+        System.out.println("Sum is " + (num1 + num2));
+    }
+}
