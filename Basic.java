6fcdccc0b35ec9241a6f57fede10b0d2823eee16
gwen041 <almiratabios01@gmail.com>
2025-12-30 22:38:44 +0800

    div of two sums

--- Basic.java
+++ Basic.java
@@ -5,12 +5,16 @@ public class Basic {
         System.out.print(name);
         System.out.println("Hello\n" + name);*/
 
-        //Addition of two num
-        int num1 = 74, num2 = 36;
+        //Addition of two nums
+        /*int num1 = 74, num2 = 36;
         int sum = num1 + num2;
         System.out.println("Sum is " + sum);
-        //System.out.println("Sum is " + (num1 + num2));
+        System.out.println("Sum is " + (num1 + num2));*/
 
+        //Division of two nums
+        int num1 = 50, num2 = 3;
+        int div = num1 / num2;
+        System.out.println(div);
 
 
 
