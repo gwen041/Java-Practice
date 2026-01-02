909db8839407059f0136865f5b4f1bc9e79a4c5c
gwen041 <almiratabios01@gmail.com>
2026-01-02 19:36:20 +0800

    product of 2 input sums

--- Basic.java
+++ Basic.java
@@ -1,4 +1,8 @@
+import java.util.Scanner;
+
 public class Basic {
+    static Scanner input = new Scanner(System.in);
+
     public static void main(String[] args) {
         /*String name = "Gwen Tabios";
         System.out.println("Hello");
@@ -16,8 +20,8 @@ public class Basic {
         int div = num1 / num2;
         System.out.println(div);*/
 
-        //Arithmetic Operations
-        int a = -5, b = 8, c = 6;
+        //ARITH OPERATIONS
+        /*int a = -5, b = 8, c = 6;
         int d = 55, e = 9;
         int f = 20, g = -3, h = 5;
         int i = 15, j = 3, k = 2;
@@ -25,8 +29,24 @@ public class Basic {
         int result2 = ((d + e) % e);
         int result3 = (f + g * h / b);
         int result4 = (h + i / j * k - b % j);
-        System.out.println(result1 + "\n" + result2 + "\n" + result3 + "\n" + result4);
+        System.out.println(result1 + "\n" + result2 + "\n" + result3 + "\n" + result4);*/
+
+        //PRODUCT OF TWO NUMS
+
+        int num1, num2, prod;
     
+        System.out.print("Input first number: ");
+        num1 = input.nextInt();
+
+        System.out.print("Input second number: ");
+        num2 = input.nextInt();
+
+        prod = num1 * num2;
+        
+        System.out.println(num1 + " x "  + num2 + " = " + prod);
+
+
+
 
         
 
