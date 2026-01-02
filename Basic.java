014ac618390f2f37333a7fd653e4d3a3e60bb0c6
gwen041 <almiratabios01@gmail.com>
2026-01-02 19:24:51 +0800

    arithmethic operations

--- Basic.java
+++ Basic.java
@@ -12,9 +12,23 @@ public class Basic {
         System.out.println("Sum is " + (num1 + num2));*/
 
         //Division of two nums
-        int num1 = 50, num2 = 3;
+        /*int num1 = 50, num2 = 3;
         int div = num1 / num2;
-        System.out.println(div);
+        System.out.println(div);*/
+
+        //Arithmetic Operations
+        int a = -5, b = 8, c = 6;
+        int d = 55, e = 9;
+        int f = 20, g = -3, h = 5;
+        int i = 15, j = 3, k = 2;
+        int result1 =  (a + b * c);
+        int result2 = ((d + e) % e);
+        int result3 = (f + g * h / b);
+        int result4 = (h + i / j * k - b % j);
+        System.out.println(result1 + "\n" + result2 + "\n" + result3 + "\n" + result4);
+    
+
+        
 
 
 
